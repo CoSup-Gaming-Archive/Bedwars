c747e737572c9fb395b39a3a03e86db65c3ff23b
Adomas <107749872+AdomasDauda@users.noreply.github.com>
2022-12-31 18:03:28 +0100

    forcestart fix

--- src/main/java/eu/cosup/bedwars/commands/ForceStartCommand.java
+++ src/main/java/eu/cosup/bedwars/commands/ForceStartCommand.java
@@ -3,12 +3,15 @@ package eu.cosup.bedwars.commands;
 import eu.cosup.bedwars.Bedwars;
 import eu.cosup.bedwars.Game;
 import eu.cosup.bedwars.managers.GameStateManager;
+import eu.cosup.bedwars.tasks.StartCountdownTask;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
+
+import java.nio.charset.CoderResult;
 
 public class ForceStartCommand implements CommandExecutor {
 
@@ -20,7 +23,7 @@ public class ForceStartCommand implements CommandExecutor {
             return true;
         }
 
-        // TODO new StartCountdownTask().runTask(Cores.getInstance());
+        new StartCountdownTask().runTask(Bedwars.getInstance());
         Bedwars.getInstance().getServer().broadcast(Component.text(sender.getName()+" issued a force start.").color(NamedTextColor.RED));
 
         return true;
