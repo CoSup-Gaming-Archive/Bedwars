b479b2e2ba4711fb96a3a30178badff52fe64c68
Adomas <107749872+AdomasDauda@users.noreply.github.com>
2023-01-01 11:39:13 +0100

    Added phases to gamestatemanager

--- src/main/java/eu/cosup/bedwars/managers/GameStateManager.java
+++ src/main/java/eu/cosup/bedwars/managers/GameStateManager.java
@@ -1,15 +1,25 @@
 package eu.cosup.bedwars.managers;
 
+
 public class GameStateManager {
 
     private GameState gameState;
+    private GamePhase gamePhase;
 
-    public void setGameState(GameState gamestate) {
-        this.gameState = gamestate;
+    public void setGameState(GameState newGameState) {
+        this.gameState = newGameState;
     }
 
     public GameState getGameState() {
         return gameState;
+    }
+
+    public void setGamePhase(GamePhase newGamePhase) {
+        this.gamePhase = newGamePhase;
+    }
+
+    public GamePhase getGamePhase() {
+        return gamePhase;
     }
 
     public enum GameState {
@@ -18,5 +28,12 @@ public class GameStateManager {
         STARTING,
         ACTIVE,
         ENDING
+    }
+
+    public enum GamePhase {
+        FIRST_UPGRADE,
+        SECOND_UPGRADE,
+        THIRD_UPGRADE,
+        SUDDEN_DEATH
     }
 }
