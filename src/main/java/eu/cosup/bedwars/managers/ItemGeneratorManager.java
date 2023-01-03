dac97a97b213c3d6188175a37cce2277a15fd034
Adomas <107749872+AdomasDauda@users.noreply.github.com>
2023-01-03 07:34:23 +0100

    Add events triggered by time to GameTimerTask

--- src/main/java/eu/cosup/bedwars/managers/ItemGeneratorManager.java
+++ src/main/java/eu/cosup/bedwars/managers/ItemGeneratorManager.java
@@ -3,6 +3,7 @@ package eu.cosup.bedwars.managers;
 import eu.cosup.bedwars.Game;
 import eu.cosup.bedwars.objects.ItemGenerator;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 
 import java.util.ArrayList;
 
@@ -22,6 +23,14 @@ public class ItemGeneratorManager {
     public void deactivateGenerators() {
         for (ItemGenerator itemGenerator : Game.getGameInstance().getSelectedMap().getItemGenerators()) {
             itemGenerator.stopDropping();
+        }
+    }
+
+    public void upgradeGenerators(Material material) {
+        for (ItemGenerator itemGenerator : Game.getGameInstance().getSelectedMap().getItemGenerators()) {
+            if (itemGenerator.getItem() == material) {
+                itemGenerator.upgrade();
+            }
         }
     }
 }
--- src/main/java/eu/cosup/bedwars/tasks/GameTimerTask.java
+++ src/main/java/eu/cosup/bedwars/tasks/GameTimerTask.java
@@ -1,14 +1,17 @@
 package eu.cosup.bedwars.tasks;
 
 import eu.cosup.bedwars.Bedwars;
+import eu.cosup.bedwars.Game;
+import eu.cosup.bedwars.managers.GameStateManager;
 import eu.cosup.bedwars.objects.SideBarInformation;
+import org.bukkit.Material;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class GameTimerTask extends BukkitRunnable {
 
     private static final int timeToFirstUpgrade = Bedwars.getInstance().getConfig().getInt("time-to-first-phase");
     private static final int timeToSecondUpgrade = Bedwars.getInstance().getConfig().getInt("time-to-second-phase");
-    private static final int timeToThirdUpgrade = Bedwars.getInstance().getConfig().getInt("time-to-third-phase");
+    private static final int timeToBedDestruction = Bedwars.getInstance().getConfig().getInt("time-to-bed-destruction-phase");
     private static final int timeToSuddenDeath = Bedwars.getInstance().getConfig().getInt("time-to-sudden-death-phase");
 
     private static int secondsElapsed;
@@ -22,6 +25,25 @@ public class GameTimerTask extends BukkitRunnable {
     public void run() {
         SideBarInformation.update();
         setSecondsElapsed(getSecondsElapsed()+1);
+
+        if (secondsElapsed == timeToFirstUpgrade) {
+            Game.getGameInstance().getGameStateManager().setGamePhase(GameStateManager.GamePhase.FIRST_UPGRADE);
+            Game.getGameInstance().getItemGeneratorManager().upgradeGenerators(Material.DIAMOND);
+        }
+
+        if (secondsElapsed == timeToSecondUpgrade) {
+            Game.getGameInstance().getGameStateManager().setGamePhase(GameStateManager.GamePhase.SECOND_UPGRADE);
+            Game.getGameInstance().getItemGeneratorManager().upgradeGenerators(Material.EMERALD);
+        }
+
+        if (secondsElapsed == timeToBedDestruction) {
+            Game.getGameInstance().getGameStateManager().setGamePhase(GameStateManager.GamePhase.BED_DESTRUCTION);
+        }
+
+        if (secondsElapsed == timeToSuddenDeath) {
+            Game.getGameInstance().getGameStateManager().setGamePhase(GameStateManager.GamePhase.SUDDEN_DEATH);
+        }
+
         new GameTimerTask().runTaskLater(Bedwars.getInstance(), 20L);
     }
 
