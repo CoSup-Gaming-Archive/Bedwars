a82476fdf3ee804b0a7499e28cfb08fe11064de6
AdomasDauda <107749872+AdomasDauda@users.noreply.github.com>
2023-01-09 19:15:12 +0000

    removed unnecesary debug line

--- src/main/java/eu/cosup/bedwars/managers/ItemGeneratorManager.java
+++ src/main/java/eu/cosup/bedwars/managers/ItemGeneratorManager.java
@@ -2,10 +2,7 @@ package eu.cosup.bedwars.managers;
 
 import eu.cosup.bedwars.Game;
 import eu.cosup.bedwars.objects.ItemGenerator;
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
-
-import java.util.ArrayList;
 
 public class ItemGeneratorManager {
 
@@ -13,7 +10,6 @@ public class ItemGeneratorManager {
     }
 
     public void activateGenerators() {
-        Bukkit.getLogger().warning("started "+Game.getGameInstance().getSelectedMap().getItemGenerators().size()+" generators");
 
         for (ItemGenerator itemGenerator : Game.getGameInstance().getSelectedMap().getItemGenerators()) {
             itemGenerator.startSpawning();
