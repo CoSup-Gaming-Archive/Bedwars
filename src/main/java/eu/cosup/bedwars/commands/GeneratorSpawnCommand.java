f54c48ae5ab0ba3e5c6427e792309f3056c3e533
Nikolas Heise <madrian.gurke@gmail.com>
2023-01-12 10:36:59 +0100

    Shorthand

--- src/main/java/eu/cosup/bedwars/commands/GeneratorSpawnCommand.java
+++ src/main/java/eu/cosup/bedwars/commands/GeneratorSpawnCommand.java
@@ -12,8 +12,10 @@ public class GeneratorSpawnCommand implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
 
-        if (sender instanceof Player) {
-            ItemGenerator itemGenerator = new ItemGenerator(((Player) sender).getLocation(), Material.EMERALD, 1, 1, 3, 0.2);
+
+
+        if (sender instanceof Player player) {
+            ItemGenerator itemGenerator = new ItemGenerator(player.getLocation(), Material.EMERALD, 1, 1, 3, 0.2);
             itemGenerator.startSpawning();
         }
 
