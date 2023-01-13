6c1dd7f576b6743952794588a2fbca39175487a3
AdomasDauda <107749872+AdomasDauda@users.noreply.github.com>
2023-01-13 18:52:22 +0000

    remove world replacement (because cloudnet use)

--- src/main/java/eu/cosup/bedwars/Bedwars.java
+++ src/main/java/eu/cosup/bedwars/Bedwars.java
@@ -12,7 +12,6 @@ import eu.cosup.bedwars.listeners.PlayerMoveListener;
 import eu.cosup.bedwars.managers.ScoreBoardManager;
 
 import eu.cosup.bedwars.objects.LoadedMap;
-import eu.cosup.bedwars.data.WorldLoader;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -27,7 +26,6 @@ public final class Bedwars extends JavaPlugin {
     private static Bedwars instance;
     private World gameWorld;
     private Game game;
-    private ArrayList<LoadedMap> loadedMaps = new ArrayList<>();
 
     public static Bedwars getInstance() {
         return instance;
@@ -44,20 +42,13 @@ public final class Bedwars extends JavaPlugin {
         getConfig().options().copyHeader(true);
         saveDefaultConfig();
 
-        loadMaps();
-
-        if (loadedMaps.size() == 0) {
-            Bukkit.getLogger().severe("Were not able to load any maps");
-            return;
-        }
-
         // initial creation of game.
         if (!createGame()) {
             return;
         }
 
         if (Game.getGameInstance().getShopManager().items.size() == 0){
-            Bukkit.getLogger().severe("We'Re not able to load the itemshop");
+            Bukkit.getLogger().severe("We'Re not able to load the itemshop canceled game");
             return;
         }
 
@@ -100,7 +91,7 @@ public final class Bedwars extends JavaPlugin {
 
 
     public boolean createGame() {
-        LoadedMap selectedMap = selectMap();
+        LoadedMap selectedMap = LoadedMap.loadMapFromConfig();
         if (selectedMap == null) {
             Bukkit.getLogger().severe("We were not able to create a game.");
             return false;
@@ -109,48 +100,6 @@ public final class Bedwars extends JavaPlugin {
         selectedMap.refreshTeamBedsFull();
         Bukkit.getLogger().warning("Succesfully started a game.");
         return true;
-    }
-
-    private LoadedMap selectMap() {
-
-        Random random = new Random();
-
-        // by default we get number 0
-        LoadedMap selectedMap;
-        selectedMap = loadedMaps.get(0);
-
-
-        // if there are more maps to choose from
-        if (loadedMaps.size() > 1) {
-            /*int selection = random.nextInt(loadedMaps.size());
-            Bukkit.getLogger().warning("Choosing from: " + loadedMaps + " chose:" + selection);
-            selectedMap = loadedMaps.get(selection);*/
-        }
-
-        Bukkit.getLogger().info("Selected map: " + selectedMap.getName());
-
-        if (!WorldLoader.loadNewWorld(selectedMap.getName())) {
-            Bukkit.getLogger().severe("Not able to load map");
-            return null;
-        }
-
-        return selectedMap;
-    }
-
-    private void loadMaps() {
-
-        // we just have to have the name of the world and then we can load it from config by name
-        ArrayList<LoadedMap> loadedMapsConfig = LoadedMap.getLoadedMapsFromConfig();
-        if (loadedMapsConfig != null) {
-            loadedMaps = loadedMapsConfig;
-        }
-
-        WorldLoader.getWorldNames();
-
-        // this is to sort out the maps that dont exist in the folder
-        List<LoadedMap> tempLoadedMaps = loadedMaps.stream().filter(loadedMap -> WorldLoader.getWorldNames().contains(loadedMap.getName())).toList();
-        loadedMaps = new ArrayList<>();
-        loadedMaps.addAll(tempLoadedMaps);
     }
 
     public World getGameWorld() {
