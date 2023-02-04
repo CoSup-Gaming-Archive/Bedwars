d5fe19b017deae5fc8031674287b58e8d1a58fce
AdomasDauda <107749872+AdomasDauda@users.noreply.github.com>
2023-02-04 15:57:43 +0000

    fix tnt interact

--- src/main/java/eu/cosup/bedwars/listeners/PlayerInteractWithEntityListener.java
+++ src/main/java/eu/cosup/bedwars/listeners/PlayerInteractWithEntityListener.java
@@ -1,10 +1,8 @@
 package eu.cosup.bedwars.listeners;
 
 import eu.cosup.bedwars.Game;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
-import org.bukkit.entity.Villager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
@@ -15,8 +13,6 @@ public class PlayerInteractWithEntityListener implements Listener {
 
         Player player=event.getPlayer();
 
-        Entity entity = event.getRightClicked();
-
         if (event.getRightClicked().getType() == EntityType.VILLAGER) {
             event.setCancelled(true);
             Game.getGameInstance().getShopManager().openShopForPlayer(player, null);
@@ -26,7 +22,5 @@ public class PlayerInteractWithEntityListener implements Listener {
             event.setCancelled(true);
             Game.getGameInstance().getUpgradesManager().openGUIForPlayer(player);
         }
-
-        Game.getGameInstance().getShopManager().openShopForPlayer(player, null);
     }
 }
