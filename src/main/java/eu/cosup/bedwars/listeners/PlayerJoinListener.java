176ffa693b8e1cc1eeabff76467d6177d5971268
AdomasDauda <107749872+AdomasDauda@users.noreply.github.com>
2023-02-05 19:11:22 +0000

    oml

--- src/main/java/eu/cosup/bedwars/listeners/PlayerJoinListener.java
+++ src/main/java/eu/cosup/bedwars/listeners/PlayerJoinListener.java
@@ -40,7 +40,7 @@ public class PlayerJoinListener implements Listener {
         if (game.getGameStateManager().getGameState() == GameStateManager.GameState.JOINING) {
             event.getPlayer().teleport(Game.getGameInstance().getSelectedMap().getSpectatorSpawn());
             if (!PlayerUtility.isPlayerStaff(event.getPlayer().getUniqueId(), event.getPlayer().getName())) {
-                new SpectatorTask(event.getPlayer(), true).runTask(Bedwars.getInstance());
+                new SpectatorTask(event.getPlayer(), false).runTask(Bedwars.getInstance());
                 return;
             }
 
