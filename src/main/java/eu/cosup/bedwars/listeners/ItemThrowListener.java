debb37fad1299862a069c7844d7a265ec6aca8b1
AdomasDauda <107749872+AdomasDauda@users.noreply.github.com>
2023-02-05 12:14:05 +0000

    prevent infinite generator stacking

--- src/main/java/eu/cosup/bedwars/listeners/ItemThrowListener.java
+++ src/main/java/eu/cosup/bedwars/listeners/ItemThrowListener.java
@@ -1,12 +1,17 @@
 package eu.cosup.bedwars.listeners;
 
+import eu.cosup.bedwars.Bedwars;
 import eu.cosup.bedwars.Game;
 import eu.cosup.bedwars.managers.GameStateManager;
+import eu.cosup.bedwars.managers.ItemGeneratorManager;
+import eu.cosup.bedwars.objects.ItemGenerator;
 import eu.cosup.bedwars.tasks.ActivateGameTask;
+import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.ItemMergeEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 
 public class ItemThrowListener implements Listener {
@@ -20,6 +25,26 @@ public class ItemThrowListener implements Listener {
         if (event.getPlayer().getGameMode() != GameMode.CREATIVE) {
             if (Game.getGameInstance().getGameStateManager().getGameState() != GameStateManager.GameState.ACTIVE) {
                 event.setCancelled(true);
+            }
+        }
+    }
+
+    @EventHandler
+    private void onItemStack(ItemMergeEvent event) {
+        for (ItemGenerator itemGenerator : Game.getGameInstance().getSelectedMap().getItemGenerators()) {
+            if (itemGenerator.getType().equals(ItemGenerator.GeneratorType.SPAWN)) {
+
+                if (event.getEntity().getItemStack().getType() == Material.GOLD_INGOT) {
+                    if (event.getTarget().getItemStack().getAmount() > 50) {
+                        event.getTarget().remove();
+                    }
+                }
+
+                if (event.getEntity().getItemStack().getType() == Material.IRON_INGOT) {
+                    if (event.getTarget().getItemStack().getAmount() > 10) {
+                        event.getTarget().remove();
+                    }
+                }
             }
         }
     }
